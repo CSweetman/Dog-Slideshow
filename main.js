fd1512bfb7dc6bb71236f96aacb4dd0adb729601
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-11-02 12:08:41 -0600

    Select options are added dynamically by the api. Started load by breed function which will eventually load pictures but for now alerts the name of the dog breed.

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello JS");
+
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await (response.json());
+    console.log(data);
+    createBreedList(data.message);
+}
+
+start();
+
+function createBreedList(breedList) {
+    document.getElementById("breed").innerHTML = `
+    <select onchange = loadByBreed(this.value)>
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList).map(breed => {
+        return `<option>${breed}</option>`
+    }).join('')}
+    </select>`
+}
+
+function loadByBreed(breed) {
+    if (breed != "Choose a dog breed") {
+        alert(breed);
+    }
+}
