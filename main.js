d1462f269082eecb18a7ec13516df96c1cb73cbd
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-11-02 14:47:03 -0600

    Added dynamic image loading to select fetch a photo from dog API of the breed that user wants. Also added basic CSS

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all");
-    const data = await (response.json());
+    const data = await response.json();
     console.log(data);
     createBreedList(data.message);
 }
@@ -18,8 +18,16 @@ function createBreedList(breedList) {
     </select>`
 }
 
-function loadByBreed(breed) {
+async function loadByBreed(breed) {
     if (breed != "Choose a dog breed") {
-        alert(breed);
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+        const data = await response.json();
+        createSlideshow(data.message);
     }
+}
+
+function createSlideshow(images) {
+    document.getElementById("slideshow").innerHTML = `
+     <div class="slide" style="background-image: url('${images[0]}')"></div>
+     `;
 }
